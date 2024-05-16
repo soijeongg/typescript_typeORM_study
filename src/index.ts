de974e5b1b2c2756e93e8da8f767e07c58183efa
soijeongg <010127js@gmail.com>
2024-05-16 21:55:49 +0900

    ✨ make basic structure

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
-import express from "express"
-import morgan  from "morgan"
-import dotenv from 'dotenv'
-import cookieParser from "cookie-parser";
+import  express from "express"
+import app from "./app"
 
-const app =express();
- app.use(express.json())
- app.use(cookieParser())
- app.use(morgan("dev"))
-
- app.get("/", (req, res)=>{
-    res.send('<h1> typeorm을 사용해보자 </h1>')
- })
 
  app.listen(process.env.PORT,()=>{
     
